7c0720edb9cb690feb4f9c64f6be6b4a724efb6e
Arnab Bala <justinarnab27@gmail.com>
2021-06-22 19:56:47 +0530

    Added the scoring mechanism

--- src/game.py
+++ src/game.py
@@ -5,6 +5,7 @@ board_size = 4	#Size of the board i.e. numbers of rows and columns
 current_score = 0  #Current score of the user
 highest_score = 0  #Highest Score of the user
 game_over = False   #Variable to indicate whether gane is over
+has_moved = False    #Indicates if the board has changed due to a move
 
 def spawn_new_cell():
 	'''Spawns a new 2 in an empty cell
@@ -42,13 +43,20 @@ def propagate(r_old,c_old,move_type):
 	if r < 0 or c < 0 or r >= board_size or c >= board_size:   #if the cell is at the edge
 		return
 	else:
+		global has_moved
 		if board[r][c] == board[r_old][c_old]: #if the adjacent cell is equal to current cell
+			global current_score
+			current_score += board[r][c] + board[r_old][c_old]
+			global highest_score
+			highest_score = max(highest_score,current_score)
 			board[r][c] += board[r_old][c_old] #add them
 			board[r_old][c_old] = 0
+			has_moved = True       #The board has changed
 		elif board[r][c] == 0:    #if the adjacent cell is empty
 			board[r][c] = board[r_old][c_old]   #propagate to next cell
 			board[r_old][c_old] = 0
 			propagate(r,c,move_type)
+			has_moved = True           #The board has changed
 
 def move(user_input):
 	'''Performs the move the user has chosen.
@@ -84,24 +92,49 @@ def display_board():
 			print(j,end=' ')
 		print()
 
+def get_highest_score():
+	'''Gets the highest score from the highest_score.txt file'''
+	file = open("highest_score.txt","r")
+	x = int(file.read())
+	file.close()
+	return x
+
+def save_highest_score(score):
+	'''Saves score as highest score by writing it in the 
+		highest_score.txt file'''
+	file = open("highest_score.txt","w")
+	file.write(str(score))
+	file.close()
+
 def play_game():
 	'''The main game loop'''
 	global game_over
 	game_over = False        #Sets the gameover variable to false
+	global current_score    
+	current_score = 0   #Sets the current_score to 0
+	global highest_score
+	highest_score = get_highest_score()
 	valid_moves = range(4)  #List of valid moves
 	board[random.randrange(board_size)][random.randrange(board_size)] = 2   #randomly assign 2 to one cell
 	while(1):
 		if game_over:
 			print("GAME OVER!")
-			return
+			break
 		display_board()   #Displays current board state
+		global has_moved
+		has_moved = False     #Resets has_moved to false at beginning of each turn
 		user_input = int(input("Your next move: "))   #Takes users input
+		if user_input == -1:  #Pressing -1 finishes the game
+			break
 		if user_input not in valid_moves:    #If users move is not valid then ignores the move
 			print("Wrong Move!")
 			continue	
 		move(user_input)    #Performs the move
-		spawn_new_cell()
-
+		if has_moved:           #Only spawns new cells if board has changed
+			spawn_new_cell()
+		print(f"Current Score {current_score}")  #Prints the current score
+		print(f"Highest Score {highest_score}")  #Prints the current score
+	save_highest_score(highest_score)
 
 
 play_game()
