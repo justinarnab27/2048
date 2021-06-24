523f4059b9d0e644c45d613723a2181b3dfe84a2
Arnab Bala <justinarnab27@gmail.com>
2021-06-24 12:50:33 +0530

    Added game over feature

--- src/game.py
+++ src/game.py
@@ -7,7 +7,14 @@ highest_score = 0  #Highest Score of the user
 game_over = False   #Variable to indicate whether gane is over
 has_moved = False    #Indicates if the board has changed due to a move
 valid_moves = range(4)  #List of valid moves
-c = 0
+
+def is_game_on():
+	'''Returns true if at least one move is possible'''
+	flag = False
+	for i in range(4):
+		flag = flag or move(i,True)    #flag becomes true if move i is possible
+	return flag
+	
 
 def spawn_new_cell():
 	'''Spawns a new 2 in an empty cell
@@ -18,12 +25,34 @@ def spawn_new_cell():
 		for j in range(board_size):
 			if board[i][j] == 0:
 				empty_cell_list.append((i,j))
-	if not empty_cell_list:		#if no empty cell, game_over is set to true
-		global game_over
-		game_over = True
+	pair = empty_cell_list[random.randrange(len(empty_cell_list))]   #randomly chooses an empty cell and then assigns 2 to it
+	board[pair[0]][pair[1]] = random.choices([2,4],weights=[9,1])[0]
+
+
+def can_propagate(r_old,c_old,move_type):
+	'''The functions check if by using a move move_type can the cell be moved
+		Returns true if can be moved'''
+	r = r_old  #The coordinates of the next adjacent cell 
+	c = c_old 
+
+	if move_type == 0:
+		r -= 1
+	elif move_type == 1:
+		c += 1
+	elif move_type == 2:
+		r += 1
 	else:
-		pair = empty_cell_list[random.randrange(len(empty_cell_list))]   #randomly chooses an empty cell and then assigns 2 to it
-		board[pair[0]][pair[1]] = random.choices([2,4],weights=[9,1])[0]
+		c -= 1
+
+	if r < 0 or c < 0 or r >= board_size or c >= board_size:   #if the cell is at the edge
+		return
+	else:
+		if board[r][c] == board[r_old][c_old]: #if the adjacent cell is equal to current cell
+			return True
+		elif board[r][c] == 0:    #if the adjacent cell is empty
+			return True
+	return False
+
 
 
 def propagate(r_old,c_old,move_type):
@@ -60,31 +89,49 @@ def propagate(r_old,c_old,move_type):
 			propagate(r,c,move_type)
 			has_moved = True           #The board has changed
 
-def move(user_input):
+def move(user_input,dont_change = False):
 	'''Performs the move the user has chosen.
-	user_input is 0 up, 1 right, 2 down, 3 left '''
+	user_input is 0 up, 1 right, 2 down, 3 left 
+	if dont_change is True it only checks if by using 
+	move user_input the board can be changed. Returns true if
+	it is possible'''
 	#Iterates over each cell and propagates it if it is not empty
+	flag = False   #flag denotes whether board can be changed
 	if user_input == 0:
 		for i in range(board_size):
 			for j in range(board_size):
 				if board[i][j] != 0:
-					propagate(i,j,0)
+					if dont_change:
+						flag = flag or can_propagate(i,j,0)   #flag becomes true if at least one cell can be moved
+					else:
+						propagate(i,j,0)
 	elif user_input == 1:
 		for i in range(board_size-1,-1,-1):
 			for j in range(board_size):
 				if board[j][i] != 0:
-					propagate(j,i,1)
+					if dont_change:
+						flag = flag or can_propagate(j,i,1)
+					else:
+						propagate(j,i,1)
+					
 	elif user_input == 2:
 		for i in range(board_size-1,-1,-1):
 			for j in range(board_size):
 				if board[i][j] != 0:
-					propagate(i,j,2)
+					if dont_change:
+						flag = flag or can_propagate(i,j,2)
+					else:
+						propagate(i,j,2)
 	else:
 		for i in range(board_size):
 			for j in range(board_size):
 				if board[j][i] != 0:
-					propagate(j,i,3)
+					if dont_change:
+						flag = flag or can_propagate(j,i,3)
+					else:
+						propagate(j,i,3)
 
+	return flag
 
 
 def display_board():
@@ -112,7 +159,7 @@ def init_game():
 	'''Initializes the game'''
 	global game_over
 	game_over = False        #Sets the gameover variable to false
-	print(id(game_over))
+	#print(id(game_over))
 	global current_score    
 	current_score = 0   #Sets the current_score to 0
 	global highest_score
@@ -123,16 +170,9 @@ def end_game():
 	'''Ends the game and saves the data'''
 	save_highest_score(highest_score)
 
-
-
 def next_turn(user_input):
 	'''The main game loop'''
 	global game_over
-	if game_over:
-		print("GAME OVER!")
-		end_game()
-		return	
-	#display_board()   #Displays current board state
 	global has_moved
 	has_moved = False     #Resets has_moved to false at beginning of each turn
 	#.user_input = int(input("Your next move: "))   #Takes users input
@@ -144,11 +184,7 @@ def next_turn(user_input):
 	move(user_input)    #Performs the move
 	if has_moved:           #Only spawns new cells if board has changed
 		spawn_new_cell()
-	global c
-	print(c,game_over,id(game_over))
-	c+=1
-	#print(f"Current Score {current_score}")  #Prints the current score
-	#print(f"Highest Score {highest_score}")  #Prints the current score
 
-
-#play_game()
+	if not is_game_on():   #If no move can change the board then game is over
+		end_game()
+		game_over = True
--- src/ui.py
+++ src/ui.py
@@ -2,11 +2,7 @@ import tkinter as tk
 from tkinter import messagebox
 import game
 
-#####################################################
-#The variables here should be imported from other files
- 
-
-##########################################################################################################
+#dict mapping cell values to background colors
 bg_color = {
 	0 : '#cc3300',
 	2 : '#ff99ff',
@@ -19,9 +15,11 @@ bg_color = {
 
 
 def update_board():
+	'''Updates the board'''
+	#iterates over whole board
 	for i in range(game.board_size):
 		for j in range(game.board_size):
-			color_background = '#cc66ff'
+			color_background = '#cc66ff'  #Default Color
 			if game.board[i][j] in bg_color:
 				color_background = bg_color[game.board[i][j]]
 			board_var_list[i][j].configure(background=color_background)
@@ -40,21 +38,12 @@ frame_grid.grid(row=0,column=0)
 frame_side = tk.Frame(master=window,bg='white') #frame for containing side pan
 frame_side.grid(row=0,column=1,sticky='nsew')
 
-#board_var_list = []
-#for i in range(game.board_size):
-#	row = []
-#	for j in range(game.board_size):
-#		board_var = tk.StringVar()
-#		row.append(board_var)
-#	board_var_list.append(row)
-
-#update_board()
-
 #creates a 3x3 grid for the game
-board_var_list = []
+board_var_list = []   #Stores all the cells
 for i in range(game.board_size):
 	row = [] 
 	for j in range(game.board_size):
+		#Creates the cells
 		box = tk.Label(master=frame_grid,width=4,height=2,borderwidth=3,relief=tk.RIDGE,font=("Arial",35))
 		row.append(box)
 		box.grid(row=i,column=j)
@@ -63,52 +52,58 @@ for i in range(game.board_size):
 score_var = tk.StringVar()
 highest_score_var = tk.StringVar()
 
+#Creates label for current score
 label_your_score = tk.Label(master=frame_side,textvariable=score_var,bg='white',font = ('Arial',12,'bold'))  #Label showing the users current score
 label_your_score.pack(pady=10,padx=5)
 
+#Creates labels for highest scores
 label_highest_score = tk.Label(master=frame_side,textvariable=highest_score_var,bg='white',font = ('Arial',12,'bold')) #Label showing the users highest score
 label_highest_score.pack(padx=5)
 
+#Creates button for resetting the highest score to 0
 button_reset_score = tk.Button(master=frame_side,text="Reset Scores") #Button for creating a new game
 button_reset_score.pack(side=tk.BOTTOM,padx=5,pady=10)
+
+#Button for restarting the game
 button_new_game = tk.Button(master=frame_side,text="New Game") #Button for creating a new game
 button_new_game.pack(side=tk.BOTTOM,padx=5,pady=10)
 
 def arrow_press(event):
-	if game.game_over:
-		print("GG")
+	'''The functions is activated is an arrow keys is pressed
+		and then updates the board'''
+	if game.game_over:      #Pressing arrow doesn't do anything if game is over
 		return
 	global score
-	#score = 30
-	score_var.set(f"Your Score:\n{game.current_score}")
-	highest_score_var.set(f"Highest Score:\n{game.highest_score}")
-	dict = {
+	score_var.set(f"Your Score:\n{game.current_score}")  #Updates the score
+	highest_score_var.set(f"Highest Score:\n{game.highest_score}")   #Updates the highest score
+	#Dict mapping keys to user_inputs
+	dict = {                   
 		"Up" : 0,
 		"Right" : 1,
 		"Down" : 2,
 		"Left" : 3
 	}
-	game.next_turn(dict[event.keysym])
-	update_board()
-	print(id(game.game_over))
-	if game.game_over:
+	game.next_turn(dict[event.keysym])   #Performs the next turn of the game
+	update_board()     #Updates the board
+	if game.game_over:        #If game is over displays game over message
 		messagebox.showinfo("Game Over","Game Over!")
-	#print(dict[event.keysym])
 
 def on_closing():
-	if messagebox.askokcancel("Quit", "Do you want to quit?"):
+	'''Activates when window is closed, saves the score and asks for confirmation'''
+	if messagebox.askokcancel("Quit", "Do you want to quit?"):  #Exit confirmation
 		window.destroy()
-	game.end_game()
+	game.end_game()   #Saves the highest score
 	return
 
+#arrow_press is called if arrow keys are pressed
 window.bind("<Up>",arrow_press)
 window.bind("<Down>",arrow_press)
 window.bind("<Left>",arrow_press)
 window.bind("<Right>",arrow_press)
 
-game.init_game()
-score_var.set(f"Your Score:\n{game.current_score}")
+game.init_game()       #Initializes the game
+score_var.set(f"Your Score:\n{game.current_score}")     #sets the score labels
 highest_score_var.set(f"Highest Score:\n{game.highest_score}")
-update_board()
-window.protocol("WM_DELETE_WINDOW",on_closing)
-window.mainloop()
+update_board()   #updates board
+window.protocol("WM_DELETE_WINDOW",on_closing)   #maps on_closing to closing window
+window.mainloop()    
